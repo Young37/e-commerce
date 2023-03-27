3f09b894ffda4aeeae1784d2475761200c78dd0f
Young37 <younghk37@gmail.com>
2023-03-27 18:16:57 +0900

    chore: Add initial project files

---
+++ src/test/java/shop/younghk37/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.younghk37.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
